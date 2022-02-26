b0c30f335284e2ad750e0d63ba8795339b8adf8c
cinntian <95114138+cinntian@users.noreply.github.com>
2022-02-26 10:13:28 +0700

    Add files via upload

---
+++ Pertemuan2/floatingpoint/FloatingPoint.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package floatingpoint;
+public class FloatingPoint {
+    public static void main(String[] args) {
+    double x = 92.98; 
+    int nx = (int) Math.round(x);
+    System.out.println("round of x is: " +nx );
+  }
+}
+
