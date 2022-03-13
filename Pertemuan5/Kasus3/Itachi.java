20c232175902740d90d25c8a8619212ed7e70eb4
cinntian <95114138+cinntian@users.noreply.github.com>
2022-03-13 22:34:16 +0700

    Add files via upload

---
+++ Pertemuan5/Kasus3/Itachi.java
@@ -0,0 +1,25 @@
+package Kasus3;
+
+/**
+ * @modification Cintia
+ */
+
+public class Itachi extends Rikudo {
+    private String KekkeiGenkai = "Susanoo";
+    private String Dojutsu = super.Dojutsu;
+    
+    void printKekkeiGenkai() {
+        System.out.println(this.KekkeiGenkai);
+    }
+    
+    void printDojutsu() {
+        System.out.println(this.Dojutsu);
+        setDojutsu();
+        System.out.println(this.Dojutsu);
+    }
+
+    private void setDojutsu() {
+        this.Dojutsu = "Mangekyou Sharingan";
+    }
+}
+
